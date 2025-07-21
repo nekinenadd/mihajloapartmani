53ad4365a7616799f898e74fab75405efc9c03bb
nekinenadd <nenad.djurdjevic2002@gmail.com>
2025-07-21 17:01:20 +0200

    s

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Navbar scroll efekat
+window.addEventListener('scroll', function () {
+  const navbar = document.getElementById('navbar');
+  if (window.scrollY > 20) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
